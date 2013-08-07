df3a8add8be9f9e2ebe7ebceb3b5e2cb3876747c
Forrest L Norvell <forrest@newrelic.com>
2013-08-06 18:30:08 -0700

    Redid error handling, ensured db connected before proceeding.
    Also cleaned up whitespace and fixed some minor jshint nits.

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ require('newrelic');
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
+  // , user = require('./routes/user')
+  , domain = require('domain')
   , http = require('http')
   , path = require('path');
 
@@ -17,14 +18,6 @@ var credentials = require('./credentials.js');
 var tweets = require('./tweets').tweets;
 
 var app = express();
-
-app.configure('development', function(){
-	  app.use(express.errorHandler({ dumpExceptions: true, showStack: true  }));
-});
-
-app.configure('production', function(){
-	  app.use(express.errorHandler()); 
-});
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -39,28 +32,45 @@ app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
-if ('development' == app.get('env')) {
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true  }));
+});
+
+app.configure('production', function(){
   app.use(express.errorHandler());
-}
-
-app.get('/', function (req, res) { tweets.all(routes.index, res); });
-app.get('/error', function() {throw new Error('This is a test')});
-
-var t = new twitter({
-	consumer_key: credentials.consumer_key,
-    	consumer_secret: credentials.consumer_secret,
-    	access_token_key: credentials.access_token_key,
-    	access_token_secret: credentials.access_token_secret
 });
 
-t.stream('statuses/filter',
-	{ track: ['awesome', 'cool', 'rad', 'gnarly', 'groovy']},
-	  function(stream) {
-		stream.on('data', function(tweet) {
-		tweets.save(tweet, null);
-		});
-	  });
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+var d = domain.create();
+d.on('error', function (error) {
+  console.error("Whoops! Got error:", error.stack);
+  process.exit(-1);
 });
+
+tweets.connect(d.intercept(function () {
+  var t = new twitter({
+    consumer_key: credentials.consumer_key,
+    consumer_secret: credentials.consumer_secret,
+    access_token_key: credentials.access_token_key,
+    access_token_secret: credentials.access_token_secret
+  });
+
+  t.stream(
+    'statuses/filter',
+    {track: ['awesome', 'cool', 'rad', 'gnarly', 'groovy']},
+    function(stream) {
+      stream.on('data', function(tweet) {
+        tweets.save(tweet, d.intercept(function () {
+          // do nothing, but let the domain handle the error
+        }));
+      });
+    }
+  );
+
+  app.get('/', function (req, res) { tweets.all(routes.index, res); });
+  app.get('/error', function() { throw new Error('This is a test'); });
+
+  http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+    console.log("waiting to start buffering tweets...");
+  });
+}));
